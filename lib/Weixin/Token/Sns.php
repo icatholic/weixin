fb7ac079d093c53abad3a90828d34672ef09150e
yangming <shanghaiyangming@gmail.com>
2014-02-27 13:52:42 +0800

    调整授权的写法

--- lib/Weixin/Token/Sns.php
+++ lib/Weixin/Token/Sns.php
@@ -1,5 +1,6 @@
 <?php
 namespace Weixin\Token;
+
 use Weixin\Exception;
 
 class Sns
@@ -13,10 +14,9 @@ class Sns
 
     private $_scope = 'snsapi_userinfo';
 
-    private $_state;
+    private $_state = '';
 
-    public function __construct ($appid, $secret, $redirect_uri, 
-            $scope = 'snsapi_userinfo', $state = '')
+    public function __construct($appid, $secret)
     {
         if (empty($appid)) {
             throw new Exception('请设定$appid');
@@ -24,28 +24,56 @@ class Sns
         if (empty($secret)) {
             throw new Exception('请设定$secret');
         }
-        if (filter_var($redirect_uri, FILTER_VALIDATE_URL) === false) {
-            throw new Exception('$redirect_uri无效');
-        }
-        if (! in_array($scope, 
-                array(
-                        'snsapi_userinfo',
-                        'snsapi_base'
-                ), true)) {
-            throw new Exception('$scope无效');
-        }
         
         $this->_appid = $appid;
         $this->_secret = $secret;
+    }
+
+    /**
+     * 设定微信回调地址
+     * 
+     * @param string $redirect_uri            
+     * @throws Exception
+     */
+    public function setRedirectUri($redirect_uri)
+    {
+        if (filter_var($redirect_uri, FILTER_VALIDATE_URL) === false) {
+            throw new Exception('$redirect_uri无效');
+        }
         $this->_redirect_uri = $redirect_uri;
+    }
+
+    /**
+     * 设定作用域类型
+     * 
+     * @param string $scope            
+     * @throws Exception
+     */
+    public function setScope($scope)
+    {
+        if (! in_array($scope, array(
+            'snsapi_userinfo',
+            'snsapi_base'
+        ), true)) {
+            throw new Exception('$scope无效');
+        }
         $this->_scope = $scope;
+    }
+
+    /**
+     * 设定携带参数信息，请使用rawurlencode编码
+     * 
+     * @param string $state            
+     */
+    public function setState($state)
+    {
         $this->_state = $state;
     }
 
     /**
      * 获取认证地址的URL
      */
-    public function getAuthorizeUrl ()
+    public function getAuthorizeUrl()
     {
         $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$this->_appid}&redirect_uri={$this->_redirect_uri}&response_type=code&scope={$this->_scope}&state={$this->_state}#wechat_redirect";
         header("location:{$url}");
@@ -58,14 +86,13 @@ class Sns
      * @throws Exception
      * @return array
      */
-    public function getAccessToken ()
+    public function getAccessToken()
     {
         $code = isset($_GET['code']) ? trim($_GET['code']) : '';
         if ($code == '') {
             throw new Exception('code不能为空');
         }
-        $response = file_get_contents(
-                "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$this->_appid}&secret={$this->_secret}&code={$code}&grant_type=authorization_code");
+        $response = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid={$this->_appid}&secret={$this->_secret}&code={$code}&grant_type=authorization_code");
         $response = json_decode($response, true);
         return $response;
     }
@@ -73,14 +100,13 @@ class Sns
     /**
      * 通过refresh token获取新的access token
      */
-    public function getRefreshToken ($refreshToken)
+    public function getRefreshToken($refreshToken)
     {
-        $response = file_get_contents(
-                "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={$this->_appid}&grant_type=refresh_token&refresh_token={$refreshToken}");
+        $response = file_get_contents("https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={$this->_appid}&grant_type=refresh_token&refresh_token={$refreshToken}");
         $response = json_decode($response, true);
         return $response;
     }
 
-    public function __destruct ()
+    public function __destruct()
     {}
 }
