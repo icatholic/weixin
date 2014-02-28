8f52224f206ebe64d42c73fadc524da028a76b38
yangming <shanghaiyangming@gmail.com>
2014-02-28 12:30:47 +0800

    去掉无效代码

--- lib/Weixin/Http/Request.php
+++ lib/Weixin/Http/Request.php
@@ -51,7 +51,6 @@ class Request
         } else {
             throw new Exception("微信服务器未有效的响应请求");
         }
-        // return json_decode(file_get_contents($this->_serviceBaseUrl . $url . '?access_token=' . $this->_accessToken . '&' . http_build_query($params)), true);
     }
 
     /**
@@ -75,13 +74,6 @@ class Request
         } else {
             throw new Exception("微信服务器未有效的响应请求");
         }
-        
-        // $url = $this->_serviceBaseUrl . $url . '?access_token=' . $this->_accessToken;
-        // $client = new \Zend_Http_Client($url);
-        // $rawData = json_encode($params, JSON_UNESCAPED_UNICODE);
-        // $client->setRawData($rawData, 'application/json');
-        // $response = $client->request('POST');
-        // return json_decode($response->getBody(), true);
     }
 
     /**
@@ -121,26 +113,6 @@ class Request
         } else {
             throw new Exception("微信服务器未有效的响应请求");
         }
-        
-        // $url = 'http://file.api.weixin.qq.com/cgi-bin/media/upload' . '?access_token=' . $this->_accessToken . '&type=' . $type;
-        // $client = new \Zend_Http_Client($url);
-        // $client->setEncType(\Zend_Http_Client::ENC_FORMDATA);
-        // if (filter_var($media, FILTER_VALIDATE_URL) !== false) {
-        // $fileInfo = $this->getFileByUrl($media);
-        // $fileName = $fileInfo['name'];
-        // $fileBytes = $fileInfo['bytes'];
-        // $client->setFileUpload($fileName, 'media', $fileBytes);
-        // } elseif (is_file($media)) {
-        // $fileBytes = file_get_contents($media);
-        // $fileName = basename($media);
-        // $client->setFileUpload($fileName, 'media', $fileBytes);
-        // } else {
-        // throw new Exception("无效的上传文件");
-        // }
-        // $response = $client->request('POST');
-        // if ($response->isSuccessful()) {
-        // return json_decode($response->getBody(), true);
-        // }
     }
 
     /**
@@ -194,29 +166,6 @@ class Request
         } else {
             throw new Exception("获取文件失败，请检查下载文件的URL是否有效");
         }
-        
-        // if (filter_var($url, FILTER_VALIDATE_URL) === false) {
-        // throw new Exception('无效的URL');
-        // }
-        // $client = new \Zend_Http_Client();
-        // $client->setUri($url);
-        // $response = $client->request('GET');
-        // if ($response->isSuccessful()) {
-        // $disposition = $response->getHeader('Content-disposition');
-        // $reDispo = '/^.*?filename=(?<f>[^\s]+|\x22[^\x22]+\x22)\x3B?.*$/m';
-        // if (preg_match($reDispo, $disposition, $mDispo)) {
-        // $filename = trim($mDispo['f'], ' ";');
-        // $fileBytes = $response->getBody();
-        // return array(
-        // 'name' => $filename,
-        // 'bytes' => $fileBytes
-        // );
-        // } else {
-        // return json_decode($response->getBody(), true);
-        // }
-        // } else {
-        // throw new Exception("获取文件失败，请检查下载文件的URL是否有效");
-        // }
     }
 
     public function __destruct()
