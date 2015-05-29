c9543f3fcfcfda46b4800fef6ec1c0e168faeca6
Guo yongrong <handsomegyr@126.com>
2015-05-29 15:09:38 +0800

    注释不必要的代码
    注释不必要的代码

--- lib/Weixin/Http/Request.php
+++ lib/Weixin/Http/Request.php
@@ -252,8 +252,10 @@ class Request
         $client->setDefaultOption('body', json_encode($params, JSON_UNESCAPED_UNICODE));
         $request = $client->post($url);
         $request->getCurlOptions()->set(CURLOPT_SSLVERSION, 1); // CURL_SSLVERSION_TLSv1
-        $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYPEER, FALSE);
-        $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYHOST, FALSE);
+                                                                
+        // $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYPEER, FALSE);
+                                                                
+        // $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYHOST, FALSE);
         
         $response = $client->send($request);
         if ($response->isSuccessful()) {
