f22ea777ddeb77a7668fea93761ac0cf8e6ea881
handsomegyr <handsomegyr@126.com>
2017-10-31 16:47:32 +0800

    发送小程序卡片,客服输入状态,一次性订阅消息,发送模板消息增加小程序信息

--- lib/Weixin/Manager/Msg/Custom.php
+++ lib/Weixin/Manager/Msg/Custom.php
@@ -204,17 +204,17 @@ class Custom
         $ret['news']['articles'] = $items;
         return $this->send($ret);
     }
-    
+
     /**
      * 发送卡券消息
      * 特别注意客服消息接口投放卡券仅支持非自定义Code码的卡券。
      *
-     * @param string $toUser
-     * @param string $card_id
-     * @param array $card_ext
+     * @param string $toUser            
+     * @param string $card_id            
+     * @param array $card_ext            
      * @return string
      */
-    public function sendWxcard($toUser, $card_id,array $card_ext)
+    public function sendWxcard($toUser, $card_id, array $card_ext)
     {
         $ret = array();
         $ret['touser'] = $toUser;
@@ -222,5 +222,79 @@ class Custom
         $ret['wxcard']['card_id'] = $card_id;
         $ret['wxcard']['card_ext'] = json_encode($card_ext);
         return $this->send($ret);
+    }
+
+    /**
+     * 发送小程序卡片（要求小程序与公众号已关联）
+     * 客户端效果如下图：
+     *
+     * 接口调用示例：
+     * {
+     * "touser":"OPENID",
+     * "msgtype":"miniprogrampage",
+     * "miniprogrampage":
+     * {
+     * "title":"title",
+     * "appid":"appid",
+     * "pagepath":"pagepath",
+     * "thumb_media_id":"thumb_media_id"
+     * }
+     * }
+     */
+    public function sendMiniProgramPage($toUser, $title, $appid, $pagepath, $thumb_media_id)
+    {
+        $ret = array();
+        $ret['touser'] = $toUser;
+        $ret['msgtype'] = 'miniprogrampage';
+        $ret['miniprogrampage']['title'] = $title;
+        $ret['miniprogrampage']['appid'] = $appid;
+        $ret['miniprogrampage']['pagepath'] = $pagepath;
+        $ret['miniprogrampage']['thumb_media_id'] = $thumb_media_id;
+        return $this->send($ret);
+    }
+
+    /**
+     * 客服输入状态
+     * 开发者可通过调用“客服输入状态”接口，返回客服当前输入状态给用户。
+     * 微信客户端效果图如下：
+     *
+     * 此接口需要客服消息接口权限。
+     * 1. 如果不满足发送客服消息的触发条件，则无法下发输入状态。
+     * 2. 下发输入状态，需要客服之前30秒内跟用户有过消息交互。
+     * 3. 在输入状态中（持续15s），不可重复下发输入态。
+     * 4. 在输入状态中，如果向用户下发消息，会同时取消输入状态。
+     * 接口调用请求说明
+     *
+     * http请求方式: POST
+     * https://api.weixin.qq.com/cgi-bin/message/custom/typing?access_token=ACCESS_TOKEN
+     * JSON数据包如下：
+     *
+     * { "touser":"OPENID", "command":"Typing"}
+     * 预期返回：
+     *
+     * { "errcode":0, "errmsg":"ok"}
+     * 参数说明
+     *
+     * 参数 是否必须 说明
+     * access_token 是 调用接口凭证
+     * touser 是 普通用户（openid）
+     * command 是 "Typing"：对用户下发“正在输入"状态 "CancelTyping"：取消对用户的”正在输入"状态
+     * 返回码说明
+     *
+     * 参数 说明
+     * 45072 command字段取值不对
+     * 45080 下发输入状态，需要之前30秒内跟用户有过消息交互
+     * 45081 已经在输入状态，不可重复下发
+     */
+    public function typing($touser, $command = 'Typing')
+    {
+        $params = array();
+        $params['touser'] = $touser;
+        if (empty($command)) {
+            $command = 'Typing';
+        }
+        $params['command'] = $command;
+        $rst = $this->_client->getRequest()->post('message/custom/typing', $params);
+        return $this->_client->rst($rst);
     }
 }
--- lib/Weixin/Manager/Msg/Template.php
+++ lib/Weixin/Manager/Msg/Template.php
@@ -31,13 +31,17 @@ class Template
      * @throws Exception
      * @return array
      */
-    public function send($touser, $template_id, $url, $topcolor, array $data)
+    public function send($touser, $template_id, $url, $topcolor, array $data, $miniprogram = NUll)
     {
         /**
          * {
          * "touser":"OPENID",
          * "template_id":"ngqIpbwh8bUfcSsECmogfXcV14J0tQlEpBO27izEYtY",
          * "url":"http://weixin.qq.com/download",
+         * "miniprogram":{
+         * "appid":"xiaochengxuappid12345",
+         * "pagepath":"index?foo=bar"
+         * },
          * "topcolor":"#FF0000",
          * "data":{
          * "first": {
@@ -66,6 +70,9 @@ class Template
         $params['touser'] = $touser;
         $params['template_id'] = $template_id;
         $params['url'] = $url;
+        if (! is_null($miniprogram)) {
+            $params['miniprogram'] = $miniprogram;
+        }
         $params['topcolor'] = $topcolor;
         $params['data'] = $data;
         $rst = $this->_client->getRequest()->post('message/template/send', $params);
@@ -91,12 +98,12 @@ class Template
      * }
      * 参数说明
      *
-     * 参数	是否必须	说明
+     * 参数 是否必须 说明
      * industry_id1 是 公众号模板消息所属行业编号
      * industry_id2 是 公众号模板消息所属行业编号
      * 行业代码查询
      *
-     * 主行业	副行业	代码
+     * 主行业 副行业 代码
      * IT科技 互联网/电子商务 1
      * IT科技 IT软件与服务 2
      * IT科技 IT硬件与设备 3
@@ -104,7 +111,7 @@ class Template
      * IT科技 通信与运营商 5
      * IT科技 网络游戏 6
      * 金融业 银行 7
-     * 金融业	基金|理财|信托 8
+     * 金融业 基金|理财|信托 8
      * 金融业 保险 9
      * 餐饮 餐饮 10
      * 酒店旅游 酒店 11
@@ -117,7 +124,7 @@ class Template
      * 政府与公共事业 学术科研 18
      * 政府与公共事业 交警 19
      * 政府与公共事业 博物馆 20
-     * 政府与公共事业	公共事业|非盈利机构 21
+     * 政府与公共事业 公共事业|非盈利机构 21
      * 医药护理 医药医疗 22
      * 医药护理 护理美容 23
      * 医药护理 保健与卫生 24
@@ -125,18 +132,18 @@ class Template
      * 交通工具 摩托车相关 26
      * 交通工具 火车相关 27
      * 交通工具 飞机相关 28
-     * 房地产	建筑 29
+     * 房地产 建筑 29
      * 房地产 物业 30
      * 消费品 消费品 31
      * 商业服务 法律 32
-     * 商业服务	会展 33
+     * 商业服务 会展 33
      * 商业服务 中介服务 34
-     * 商业服务	认证 35
-     * 商业服务	审计 36
-     * 文体娱乐	传媒 37
+     * 商业服务 认证 35
+     * 商业服务 审计 36
+     * 文体娱乐 传媒 37
      * 文体娱乐 体育 38
      * 文体娱乐 娱乐休闲 39
-     * 印刷	印刷 40
+     * 印刷 印刷 40
      * 其它 其它 41
      *
      *
@@ -166,8 +173,8 @@ class Template
      * https://api.weixin.qq.com/cgi-bin/template/get_industry?access_token=ACCESS_TOKEN
      * 参数说明
      *
-     * 参数	是否必须	说明
-     * access_token	是	接口调用凭证
+     * 参数 是否必须 说明
+     * access_token 是 接口调用凭证
      * 返回说明
      *
      * 正确调用后的返回示例：
@@ -178,9 +185,9 @@ class Template
      * }
      * 返回参数说明
      *
-     * 参数	说明
-     * primary_industry	帐号设置的主营行业
-     * secondary_industry	帐号设置的副营行业
+     * 参数 说明
+     * primary_industry 帐号设置的主营行业
+     * secondary_industry 帐号设置的副营行业
      */
     public function getIndustry()
     {
@@ -207,7 +214,7 @@ class Template
      * }
      * 参数说明
      *
-     * 参数	是否必须	说明
+     * 参数 是否必须 说明
      * template_id_short 是 模板库中模板的编号，有“TM**”和“OPENTMTM**”等形式
      * 返回码说明
      *
@@ -238,8 +245,8 @@ class Template
      * https://api.weixin.qq.com/cgi-bin/template/get_all_private_template?access_token=ACCESS_TOKEN
      * 参数说明
      *
-     * 参数	是否必须	说明
-     * access_token	是	接口调用凭证
+     * 参数 是否必须 说明
+     * access_token 是 接口调用凭证
      * 返回说明
      *
      * 正确调用后的返回示例：
@@ -256,13 +263,13 @@ class Template
      * }
      * 返回参数说明
      *
-     * 参数	说明
-     * template_id	模板ID
-     * title	模板标题
-     * primary_industry	模板所属行业的一级行业
-     * deputy_industry	模板所属行业的二级行业
-     * content	模板内容
-     * example	模板示例
+     * 参数 说明
+     * template_id 模板ID
+     * title 模板标题
+     * primary_industry 模板所属行业的一级行业
+     * deputy_industry 模板所属行业的二级行业
+     * content 模板内容
+     * example 模板示例
      */
     public function getAllPrivateTemplate()
     {
@@ -287,8 +294,8 @@ class Template
      * }
      * 参数说明
      *
-     * 参数	是否必须	说明
-     * template_id	是	公众帐号下模板消息ID
+     * 参数 是否必须 说明
+     * template_id 是 公众帐号下模板消息ID
      * 返回说明
      *
      * 在调用接口后，会返回JSON数据包。正常时的返回JSON数据包示例：
@@ -302,6 +309,92 @@ class Template
         $params = array();
         $params['template_id'] = $template_id;
         $rst = $this->_client->getRequest()->post('template/del_private_template', $params);
+        return $this->_client->rst($rst);
+    }
+
+    /**
+     * 一次性订阅消息
+     * 开发者可以通过一次性订阅消息授权让微信用户授权第三方移动应用（接入说明）或公众号，获得发送一次订阅消息给到授权微信用户的机会。授权微信用户可以不需要关注公众号。微信用户每授权一次，开发者可获得一次下发消息的权限。对于已关注公众号的，消息将下发到公众号会话；未关注公众号的，将下发到服务通知。
+     *
+     * 公众号或网页使用一次性订阅消息流程如下：
+     * 第一步：需要用户同意授权，获取一次给用户推送一条订阅模板消息的机会
+     * 在确保微信公众帐号拥有订阅消息授权的权限的前提下（已认证的公众号即有权限，可登陆公众平台在接口权限列表处查看），引导用户在微信客户端打开如下链接：
+     * https://mp.weixin.qq.com/mp/subscribemsg?action=get_confirm&appid=wxaba38c7f163da69b&scene=1000&template_id=1uDxHNXwYQfBmXOfPJcjAS3FynHArD8aWMEFN
+     * RGSbCc& redirect_url=http%3a%2f%2fsupport.qq.com&reserved=test#wechat_redirect
+     * 参数说明
+     * 参数
+     * 是否必须 说明
+     * action 是 直接填get_confirm即可
+     * appid 是
+     * 公众号的唯一标识
+     * scene 是 重定向后会带上scene参数，开发者可以填0-10000的整形值，用来标识订阅场景值
+     * template_id 是 订阅消息模板ID，登录公众平台后台，在接口权限列表处可查看订阅模板ID
+     * redirect_url 是 授权后重定向的回调地址，请使用UrlEncode对链接进行处理。注：要求redirect_url的域名要跟登记的业务域名一致，且业务域名不能带路径。业务域名需登录公众号，在设置-公众号设置-功能设置里面对业务域名设置。
+     * reserved 否 用于保持请求和回调的状态，授权请后原样带回给第三方。该参数可用于防止csrf攻击（跨站请求伪造攻击），建议第三方带上该参数，可设置为简单的随机数加session进行校验，开发者可以填写a-zA-Z0-9的参数值，最多128字节，要求做urlencode
+     * #wechat_redirect 是 无论直接打开还是做页面302重定向时，必须带此参数
+     * 用户同意或取消授权后会返回相关信息
+     * 如果用户点击同意或取消授权，页面将跳转至：
+     * redirect_url/?openid=OPENID&template_id=TEMPLATE_ID&action=ACTION&scene=SCENE
+     * 参数说明
+     * 参数 说明
+     * openid 用户唯一标识，只在用户确认授权时才会带上
+     * template_id 订阅消息模板ID
+     * action 用户点击动作，”confirm”代表用户确认授权，”cancel”代表用户取消授权
+     * scene 订阅场景值
+     * reserved 请求带入原样返回
+     */
+    public function getAuthorizeUrl4Subscribemsg($appid, $template_id, $scene, $redirect_url, $reserved, $action = 'get_confirm')
+    {
+        $redirect_url = urlencode($redirect_url);
+        $url = "https://mp.weixin.qq.com/mp/subscribemsg?action={$action}&appid={$appid}&scene={$scene}&template_id={$template_id}&redirect_url={$redirect_url}&reserved={$reserved}#wechat_redirect";
+        return $url;
+    }
+
+    /**
+     * 第二步：通过API推送订阅模板消息给到授权微信用户
+     * 接口请求说明
+     * http请求方式: post
+     * https://api.weixin.qq.com/cgi-bin/message/template/subscribe?access_token=ACCESS_TOKEN
+     * post数据示例
+     * {
+     * “touser”:”OPENID”,
+     * “template_id”:”TEMPLATE_ID”,
+     * “url”:”URL”,
+     * “scene”:”SCENE”,
+     * “title”:”TITLE”,
+     * “data”:{
+     * “content”:{
+     * “value”:”VALUE”,
+     * “color”:”COLOR”
+     * }
+     * }
+     * }
+     * 参数说明
+     * 参数 是否必须 说明
+     * touser 是 填接收消息的用户openid
+     * template_id 是 订阅消息模板ID
+     * url 否 点击消息跳转的链接，需要有ICP备案
+     * scene 是 订阅场景值
+     * title 是 消息标题，15字以内
+     * data 是 消息正文，value为消息内容文本（200字以内），没有固定格式，可用\n换行，color为整段消息内容的字体颜色（目前仅支持整段消息为一种颜色）
+     * 返回说明
+     * 在调用接口后，会返回JSON数据包。正常时的返回JSON数据包示例：
+     * {
+     * “errcode”:0,
+     * “errmsg”:”ok”
+     * }
+     */
+    public function subscribe($touser, $template_id, $url, $scene, $title, array $data)
+    {
+        $params = array();
+        $params['touser'] = $touser;
+        $params['template_id'] = $template_id;
+        $params['url'] = $url;
+        $params['scene'] = $scene;
+        $params['title'] = $title;
+        $params['data'] = $data;
+        
+        $rst = $this->_client->getRequest()->post('message/template/subscribe', $params);
         return $this->_client->rst($rst);
     }
 }
